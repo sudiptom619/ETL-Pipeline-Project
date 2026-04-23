3166cc8e38f6e6885522f75a2a6aa6780568c0ab
Sudipto <sudiptom619@gmail.com>
2026-04-23 22:59:29 +0530

    insert invoices table from local db to aws rds

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import os
 import pandas as pd
 import numpy as np
+from pandas.core.dtypes.common import is_unsigned_integer_dtype
+from pandas.io.sql import to_sql
 import mysql.connector
 from sqlalchemy import create_engine
 
@@ -8,19 +10,16 @@ password = os.environ["My_SQL"]
 
 
 def connect_aws():
-    conn = mysql.connector.connect(
-        host="database-1.c5symesi6dhr.eu-north-1.rds.amazonaws.com",
-        user="admin",
-        password=password,
-        database="test",
+    engine = create_engine(
+        f"mysql+mysqlconnector://admin:{password}@database-1.c5symesi6dhr.eu-north-1.rds.amazonaws.com:3306/test"
     )
 
-    return conn
+    return engine
 
 
 def connect_mysql():
     engine = create_engine(
-        f"mysql+mysqlconnector://root:{password}%40@0.tcp.in.ngrok.io:13999/supermarket "
+        f"mysql+mysqlconnector://root:{password}%40@0.tcp.in.ngrok.io:11099/supermarket "
     )
     return engine
 
@@ -31,10 +30,15 @@ def fetch_table(table_name):
     return df
 
 
+def insert_table_to_aws(df, table_name):
+    df.to_sql(name=table_name, con=aws_engine, if_exists="append", index=False)
+
+
+aws_engine = connect_aws()
 mysql_engine = connect_mysql()
 invoices = fetch_table("invoices")
 orderleads = fetch_table("orderleads")
 salesteam = fetch_table("salesteam")
-
+insert_table_to_aws(invoices, "invoices")
 
 print(invoices.shape, orderleads.shape, salesteam.shape, sep=" ")
